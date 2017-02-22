c1676dca2b7742c65ae377052cc8bfc10d2f3e15
Param Dhaliwal <paramdhal@gmail.com>
2017-02-22 16:54:10 +0000

    Dot notation

--- src/NViewController.php
+++ src/NViewController.php
@@ -5,6 +5,7 @@ namespace RS\NView;
 use Illuminate\Contracts\View\View as ViewContract;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Contracts\Translation\Translator;
+use Illuminate\Support\Collection;
 
 class NViewController implements ViewContract {
 
@@ -43,6 +44,13 @@ class NViewController implements ViewContract {
 	 */
 	protected $path;
 
+	/**
+	 * The prefix for all xpaths.
+	 *
+	 * @var string
+	 */
+	protected $prefix = "data-v.";
+
 
 	/**
 	 * Create a new view instance.
@@ -61,7 +69,8 @@ class NViewController implements ViewContract {
 	}
 
 	public function render() {
-		
+
+		$this->compileChildGap();
 		$this->compileTranslations();
 
 		return $this->view->show(true);
@@ -89,14 +98,76 @@ class NViewController implements ViewContract {
 	}
 
 	protected function compileTranslations(){
-		$nodes = $this->view->getList('//*[@data-v.tr]');
+		$nodes = $this->getNodesByToken('tr');
 
 		foreach ($nodes as $node){
 			$translation = $this->translator->trans(
-			  $node->getAttribute('data-v.tr')
+			  $node->getAttribute("{$this->prefix}tr")
 			);
 			$this->view->set('.',$translation,$node);
 		}
+	}
+
+	protected function compileChildGap(){
+		$nodes = $this->getNodesByToken('child');
+
+		$data = $this->data;
+
+		$data = collect(array_dot($data));
+
+		foreach ($nodes as $node){
+			$attribute = $node->getAttribute("{$this->prefix}child");
+
+			if($data->has($attribute)){
+				$value = $data->get($attribute);
+			}else{
+				$composite = array_reverse(explode('.',$attribute));
+				$accessor = array_pop($composite);
+
+				if($data->has($accessor)){
+					$value = $this->getValue($composite,$data->get($accessor));
+				}else{
+					$value = $attribute;
+				}
+
+			}
+
+			$this->view->set('./child-gap()',$value,$node);
+		}
+	}
+
+	/**
+	 * Gets all the nodes for the given token
+	 *
+	 * @param string $token
+	 * @return \DOMNodeList
+	 */
+	protected function getNodesByToken(string $token): \DOMNodeList{
+		return $this->view->getList("//*[@{$this->prefix}{$token}]");
+	}
+
+	protected function getValue($composite,$data){
+
+		// There are no more pieces left to access so return the result
+		if(count($composite) == 0){
+			return $data;
+		}
+
+		// Get the next accessor
+		$accessor = array_pop($composite);
+
+		// If the current data is an object then get the property of the object
+		if(gettype($data) == "object"){
+			return $this->getValue($composite,$data->$accessor);
+		}
+
+		// If the current data is an array then get the key of the array
+		if(gettype($data) == "array"){
+			$collection = collect(array_dot($data));
+			return $this->getValue($composite,$collection->get($accessor));
+		}
+
+
 	}
 
 }
