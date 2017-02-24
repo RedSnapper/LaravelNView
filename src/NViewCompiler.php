458e6cf5b49203771181b286874c4e4a623f5e87
Param Dhaliwal <paramdhal@gmail.com>
2017-02-24 13:37:00 +0000

    Only compile found tokens

--- src/NViewCompiler.php
+++ src/NViewCompiler.php
@@ -63,6 +63,14 @@ class NViewCompiler implements ViewContract {
 	 */
 	protected $controller;
 
+	protected $compilers = [
+	  ['token' => 'can', 'function' => 'Can'],
+	  ['token' => 'can', 'function' => 'Can'],
+	  ['token' => 'child', 'function' => 'ChildGap'],
+	  ['token' => 'text', 'function' => 'Text'],
+	  ['token' => 'tr', 'function' => 'Translations'],
+	];
+
 	/**
 	 * Create a new view instance.
 	 *
@@ -71,14 +79,13 @@ class NViewCompiler implements ViewContract {
 	 * @param  string $path
 	 * @param  mixed  $data
 	 */
-	public function __construct(Factory $factory,$viewName, string $path, $data = []) {
+	public function __construct(Factory $factory, $viewName, string $path, $data = []) {
 		$this->view = new NView($path);
 		$this->factory = $factory;
 		$this->container = $this->factory->getContainer();
 		$this->viewName = $viewName;
 		$this->path = $path;
 		$this->data = $data instanceof Arrayable ? $data->toArray() : (array)$data;
-
 	}
 
 	public function render() {
@@ -87,16 +94,11 @@ class NViewCompiler implements ViewContract {
 		return $view->show(true);
 	}
 
-	public function compile(){
+	public function compile() {
 
 		$this->loadViewController($this->viewName);
 
-		$collection = collect(array_dot($this->data));
-
-		$this->compileCan();
-		$this->compileChildGap($collection);
-		$this->compileText($collection);
-		$this->compileTranslations();
+		$this->runCompilers();
 
 		$this->renderViewController();
 		$this->renderParent();
@@ -119,9 +121,10 @@ class NViewCompiler implements ViewContract {
 
 	/**
 	 * Has a controller
+	 *
 	 * @return bool
 	 */
-	public function hasController():bool {
+	public function hasController(): bool {
 		return !is_null($this->controller);
 	}
 
@@ -142,77 +145,82 @@ class NViewCompiler implements ViewContract {
 		return $this;
 	}
 
-	protected function compileTranslations(){
+	protected function runCompilers() {
 
-		$translator = $this->container->make('translator');
+		$collection = collect(array_dot($this->data));
 
-		$this->compileNodes('tr',function(\DOMElement $node,$attribute) use ($translator){
+		$tokens = $this->getTokensFromView();
+		
+		foreach ($this->compilers as $compiler) {
 
-			$translation = $translator->trans($attribute);
-
-			$this->view->set('.',$translation,$node);
-
-		});
-
-	}
-
-	protected function compileChildGap(Collection $data){
-
-		$this->compileNodes('child',function(\DOMElement $node,$attribute) use ($data){
-
-			$value = $this->getValue($attribute,$data);
-
-			$this->view->set('./child-gap()',$value,$node);
-
-		});
-
-	}
-
-	protected function compileText(Collection $data){
-
-		$this->compileNodes('text',function(\DOMElement $node,$attribute) use ($data){
-
-			$value = $this->getValue($attribute,$data);
-
-			$this->view->set('.',$value,$node);
-
-		});
-	}
-
-	protected function compileCan() {
-
-		$gate = $this->container->make('Gate');
-
-		$this->compileNodes('can',function(\DOMElement $node,$attribute) use($gate){
-
-			if($gate::denies($attribute)){
-				$this->view->set('.',null,$node);
-			};
-
-		});
-
-	}
-
-	protected function compileNodes($token,\Closure $closure){
-		$nodes = $this->getNodesByToken($token);
-
-		$closure = $closure->bindTo($this);
-
-		foreach ($nodes as $node){
-
-			$attribute = $this->getNodeAttribute($node,$token);
-			$closure($node,$attribute);
+			if(in_array($compiler['token'],$tokens)){
+				$compiler = "compile{$compiler['function']}";
+				$this->$compiler($collection);
+			}
 
 		}
 	}
 
+	protected function compileTranslations(Collection $collection) {
 
-	protected function getNodeAttribute(\DOMElement $node,$attribute){
+		$translator = $this->container->make('translator');
 
-		return $node->getAttribute("{$this->prefix}{$attribute}");
+		$this->compileNodes('tr', function (\DOMElement $node, $attribute) use ($translator) {
 
+			$translation = $translator->trans($attribute);
+
+			$this->view->set('.', $translation, $node);
+		});
 	}
 
+	protected function compileChildGap(Collection $data) {
+
+		$this->compileNodes('child', function (\DOMElement $node, $attribute) use ($data) {
+
+			$value = $this->getValue($attribute, $data);
+
+			$this->view->set('./child-gap()', $value, $node);
+		});
+	}
+
+	protected function compileText(Collection $data) {
+
+		$this->compileNodes('text', function (\DOMElement $node, $attribute) use ($data) {
+
+			$value = $this->getValue($attribute, $data);
+
+			$this->view->set('.', $value, $node);
+		});
+	}
+
+	protected function compileCan(Collection $data) {
+
+		$gate = $this->container->make('Gate');
+
+		$this->compileNodes('can', function (\DOMElement $node, $attribute) use ($gate) {
+
+			if ($gate::denies($attribute)) {
+				$this->view->set('.', null, $node);
+			};
+		});
+	}
+
+	protected function compileNodes($token, \Closure $closure) {
+		$nodes = $this->getNodesByToken($token);
+
+		$closure = $closure->bindTo($this);
+
+		foreach ($nodes as $node) {
+
+			$attribute = $this->getNodeAttribute($node, $token);
+			$closure($node, $attribute);
+		}
+	}
+
+	protected function getNodeAttribute(\DOMElement $node, $attribute) {
+
+		return $node->getAttribute("{$this->prefix}{$attribute}");
+	}
 
 	/**
 	 * Get value from the data based on dot notation
@@ -221,17 +229,17 @@ class NViewCompiler implements ViewContract {
 	 * @param Collection $data
 	 * @return mixed|string
 	 */
-	protected function getValue(string $attribute, Collection $data):string{
+	protected function getValue(string $attribute, Collection $data): string {
 
-		if($data->has($attribute)){
+		if ($data->has($attribute)) {
 			return $data->get($attribute);
-		}else{
-			$composite = array_reverse(explode('.',$attribute));
+		} else {
+			$composite = array_reverse(explode('.', $attribute));
 			$accessor = array_pop($composite);
 
-			if($data->has($accessor)){
-				return $this->getValueFromData($composite,$data->get($accessor));
-			}else{
+			if ($data->has($accessor)) {
+				return $this->getValueFromData($composite, $data->get($accessor));
+			} else {
 				return $attribute;
 			}
 		}
@@ -243,10 +251,9 @@ class NViewCompiler implements ViewContract {
 	 * @param string $token
 	 * @return \DOMNodeList
 	 */
-	protected function getNodesByToken(string $token): \DOMNodeList{
+	protected function getNodesByToken(string $token): \DOMNodeList {
 
 		return $this->view->getList("//*[@{$this->prefix}{$token}]");
-
 	}
 
 	/**
@@ -256,10 +263,10 @@ class NViewCompiler implements ViewContract {
 	 * @param mixed $data
 	 * @return mixed
 	 */
-	protected function getValueFromData($composite, $data){
+	protected function getValueFromData($composite, $data) {
 
 		// There are no more pieces left to access so return the result
-		if(count($composite) == 0){
+		if (count($composite) == 0) {
 			return $data;
 		}
 
@@ -267,57 +274,68 @@ class NViewCompiler implements ViewContract {
 		$accessor = array_pop($composite);
 
 		// If the current data is an object then get the property of the object
-		if(gettype($data) == "object"){
-			return $this->getValueFromData($composite,$data->$accessor);
+		if (gettype($data) == "object") {
+			return $this->getValueFromData($composite, $data->$accessor);
 		}
 
 		// If the current data is an array then get the key of the array
-		if(gettype($data) == "array"){
+		if (gettype($data) == "array") {
 			$collection = collect(array_dot($data));
-			return $this->getValueFromData($composite,$collection->get($accessor));
+			return $this->getValueFromData($composite, $collection->get($accessor));
 		}
-
 	}
 
 	protected function loadViewController(string $viewName) {
 
 		$class = "App\\View\\" . studly_case($viewName);
-		if(class_exists($class)) {
+		if (class_exists($class)) {
 			$this->controller = $this->container->make($class);
 			$this->controller->setData($this->data);
 		}
 	}
 
-
 	protected function renderParent() {
-		if($this->hasController() && $this->controller->hasParent()){
+		if ($this->hasController() && $this->controller->hasParent()) {
 			return $this->view = $this->renderParentView($this->controller->getParent());
 		}
 
-		if($parent = $this->view->get("/*/@{$this->prefix}container")){
+		if ($parent = $this->view->get("/*/@{$this->prefix}container")) {
 			return $this->view = $this->renderParentView($parent);
 		}
-
-
 	}
 
-	protected function renderParentView(string $viewName):NView{
-		$parent = $this->factory->make($viewName,$this->data);
+	protected function renderParentView(string $viewName): NView {
+		$parent = $this->factory->make($viewName, $this->data);
 		$view = $parent->compile();
-		if($parent->hasController()){
-			$view = $parent->getController()->renderChild($view,$this->view);
+		if ($parent->hasController()) {
+			$view = $parent->getController()->renderChild($view, $this->view);
 		}
 		return $view;
 	}
 
 	protected function renderViewController() {
-		if($this->hasController()){
+		if ($this->hasController()) {
 			return $this->view = $this->controller->render($this->view);
 		}
 	}
 
-	protected function tidy(){
+	protected function tidy() {
 		$this->view->set("//*/@*[starts-with(name(),'$this->prefix')]");
+	}
+
+	protected function getTokensFromView():array{
+
+		$attributes = $this->view->getList("//*/@*[starts-with(name(),'$this->prefix')]");
+
+		$tokens = [];
+
+		foreach ($attributes as $attribute) {
+			if (starts_with($attribute->name, $this->prefix)) {
+				$tokens[] = substr($attribute->name, mb_strlen($this->prefix));
+			}
+		}
+
+		return array_unique($tokens);
 	}
 
 }
