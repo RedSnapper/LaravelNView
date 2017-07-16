bfa6c1ea562ded1429f29dfbc8557eb919cbe05f
Param Dhaliwal <paramdhal@gmail.com>
2017-07-16 11:12:52 +0100

    Added some attribute tests

--- tests/src/DocumentTest.php
+++ tests/src/DocumentTest.php
@@ -8,7 +8,7 @@ class DocumentTest extends TestCase {
 
 
 	function testShow(){
-		$v = new Document('<!DOCTYPE div><div xmlns="http://www.w3.org/1999/xhtml"/>');
+		$v = $this->getView();
 		$this->assertEquals('<div></div>',$v->show());
 
 		$expected = <<<EOT
@@ -23,7 +23,52 @@ EOT;
 	}
 
 	public function testSetAttribute() {
-		
+
+		$v = $this->getView();
+		$v->set('./@id',"foo");
+		$this->assertEquals('<div id="foo"></div>',$v->show(),"Should be able to add attribute to element");
+
+		$v = $this->getView();
+		$v->set('./@id',"");
+		$this->assertEquals('<div id=""></div>',$v->show(),"Should be able to add attribute with an empty string value");
+
+		$v = $this->getView(['id'=>'foo']);
+		$v->set('./@id',"bar");
+		$this->assertEquals('<div id="bar"></div>',$v->show(),"Should be able to change attribute value");
+
+		$v = $this->getView(['id'=>'foo']);
+		$v->set('./@id');
+		$this->assertEquals('<div></div>',$v->show(),"Should be able to delete attribute");
+
+		$v = $this->getView(['id'=>'foo']);
+		$v->set('./@id/child-gap()',"bar");
+		$this->assertEquals('<div id="foobar"></div>',$v->show(),"Should be able to append to the attribute value");
+
+		$v = $this->getView(['id'=>'foo']);
+		$v->set('./@id/preceding-gap()',"bar");
+		$this->assertEquals('<div id="barfoo"></div>',$v->show(),"Should be able to prepend to the attribute value");
+
+		$v = $this->getView(['id'=>'foo']);
+		$v->set('./@id/following-gap()',"bar");
+		$this->assertEquals('<div id="foobar"></div>',$v->show(),"Should be able to append to the attribute value");
+
+		$v = $this->getView();
+		$v->set('./@id',"foo&bar");
+		$this->assertEquals('<div id="foo&amp;bar"></div>',$v->show(),"Should xml encode attributes");
+	}
+
+	private function getView(array $attributes=[]){
+
+		$attributes = $this->attributes($attributes);
+
+		return new Document('<!DOCTYPE div><div ' . $attributes . ' xmlns="http://www.w3.org/1999/xhtml"/>');
+	}
+
+	private function attributes(array $attributes=[]){
+
+		return array_reduce(array_keys($attributes),function($carry,$key) use($attributes){
+			return $carry . ' ' . $key . '="' . $attributes[$key] . '"';
+		},"");
 	}
 
 }
